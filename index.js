4d92a1ee4a03e86760473b39cc54dcb657b92d69
pentium-tony <tony.luo@pentium.network>
2019-10-17 14:05:59 +0800

    misc: Provide config

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const GeneralLunar = require('./GeneralLunar');
 const BasicLunar = require('./BasicLunar');
 const AdvancedLunar = require('./AdvancedLunar');
 const ApplicationLunar = require('./ApplicationLunar');
+const config = require('./config');
 
 module.exports = {
     GeneralLunar,
     BasicLunar,
     AdvancedLunar,
     ApplicationLunar,
+    config,
 };
